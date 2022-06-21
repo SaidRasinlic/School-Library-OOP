a1d603bc0a153446bdc7a54da083bcc80f47de3c
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-21 13:51:28 +0200

    Add Person file with it's state and behaviour

--- Person.rb
+++ Person.rb
@@ -3,7 +3,7 @@ class Person
   attr_accessor :age, :name
   attr_reader :id
 
-  def initialize(name = "Unknown", age, parent_permission = true)
+  def initialize(name = "Unknown", age, parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
