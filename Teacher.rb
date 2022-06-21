beaf23c3c940527df52210ebc49920cd329ec0b0
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-21 14:21:29 +0200

    Add Teacher file with it's state and behaviour

--- Teacher.rb
+++ Teacher.rb
@@ -3,7 +3,7 @@ require_relative "person"
 class Teacher < Person
 
   def initialize(name = "Unknown", age, parent_permission: true, specialization)
-    super(name, age, parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
