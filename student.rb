156c472796754f9955c34a0baf9946030ca6b920
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-25 12:35:58 +0200

    Add Student file with it's state, behaviour and relationship

--- student.rb
+++ student.rb
@@ -1,9 +1,17 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  # belongs to relationship
+  attr_reader :classroom
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+  end
+
+  # both ways relationship
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
