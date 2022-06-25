0aa89c09f9d9ebabf13f3078172bd1b3ef003906
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-06-25 12:36:45 +0200

    Add Book file with it's state, behaviour and relationship

---
+++ book.rb
@@ -0,0 +1,17 @@
+class Book
+  attr_accessor :title, :author, :rentals
+  # belongs to relationship
+  # attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  # has many + both ways relationship
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+end
